c9ff2863b03db6e50cccfd6535ff6bf0c3b7037d
류준영 <119715555+SLIPPECAT@users.noreply.github.com>
2024-12-02 08:31:58 +0900

    Update streamlit_deploy.py

--- streamlit_deploy.py
+++ streamlit_deploy.py
@@ -2,14 +2,14 @@ import os
 import streamlit as st
 import tempfile
 
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.vectorstores import Chroma
+# from langchain.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
